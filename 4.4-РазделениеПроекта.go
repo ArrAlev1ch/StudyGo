a391bf63211607779fc13b75c997fa1eb10e60ac
ArrAlev1ch <ismagilovar2005@gmail.com>
2026-08-13 22:04:32 +0300

    Update 4.4-РазделениеПроекта.go

--- 4.4-РазделениеПроекта.go
+++ 4.4-РазделениеПроекта.go
@@ -9,3 +9,17 @@ func main() {
 	fmt.Println(utils.Add(3, 4))
 	fmt.Println(utils.Multiply(3, 4))
 }
+
+/* в папке src/TEST/mathutils, файл math.go
+
+package utils
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
+*/
